d8a44e4f29ab4057beedab3228a224cde6e1a6c0
peawyoyoyin <peawyoyoyin@gmail.com>
2019-03-02 17:01:23 +0700

    add more content, change to jquery-scrollify instead of react-snap-scroll to support firefox

---
+++ frontend/src/footer-authors.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FooterAuthors = () => (
+  <div>
+    Website designed by Athipat Nampetch, made with love by @peawyoyoyin, @maxminor, @phootip
+  </div>
+)
+
+export default FooterAuthors;
--- frontend/src/full-screen.js
+++ frontend/src/full-screen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const FullScreen = ({className = "", children}) => (
-  <div className={"fullscreen " + className}>
+  <div className={"fullscreen " + className}
+    style={{width: '99vw', height: '98vh'}}
+  >
     {children}
   </div>
 )
