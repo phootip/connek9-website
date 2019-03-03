033a9ffdc91338b99f912167816d6b238b12ceca
Phootip Tulayavorases <phootip.t@gmail.com>
2019-03-04 00:01:38 +0700

    add reason-to-come contents

--- frontend/src/reasons-to-come.js
+++ frontend/src/reasons-to-come.js
@@ -7,17 +7,24 @@ const ReasonsToCome = () => (
       <div className="about header container"
         style={{display: 'flex', flex: 1, justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}
       >
-        <div className="splash logo header-text">
-          <h1> Reasons to Come </h1>
+        <div className="animatedParent">
+          <div className="splash logo animated fadeInUpShort header-text">
+            <h1> Reason to come </h1>
+          </div>
         </div>
       </div>
       <div className="about text container"
         style={{display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '2em'}}
       >
         <div className='animatedParent animateOnce'>
-          <p className="animated bounceInRight content-text">
-            สมิติเวชตี๋พุทโธ ไลน์เฟรช ปอดแหกเที่ยงคืนตรวจสอบ ชาร์ปโคโยตีแคปเซนเซอร์เตี๊ยม ไฟแนนซ์บุญคุณแซลมอนเวณิกา นายพราน ออร์แกนิกแดนซ์เลคเชอร์ ไกด์วอร์รูม อีโรติกวอฟเฟิล โพลารอยด์ยะเยือกโมจิ เคลมนิรันดร์นางแบบ พีเรียดคอนโด โซน เด้อคอลัมน์ โอวัลติน แรงผลักเทียมทานแอ็คชั่นว่ะเพียว
-          </p>
+          <div className="animated bounceInRight content-text">
+            <ul>
+              <li>ได้เจอเพื่อนๆ พี่ๆ ร่วมชะตากรรมในสายงานเดียวกัน แบ่งปันประสบการณ์เพิ่ม <u><b>connek</b></u>tion ให้กับตัวเอง</li>
+              <li>มีกิจกรรม ใช้แรงใช้ความคิดประลองปัญญา? มั้งไม่รู้อะต้องถามฝ่ายกิจดู แต่ที่แน่ๆคือสนุกแน่นอน</li>
+              <li>มีของฟรี(ใครๆก็ชอบ)แจกจากสปอนเซอร์ โดยเฉพาะสติกเกอร์สุด Geek เอาไปแปะคอมแก้เบื่อเพื่อประกาศให้โลกรู้ว่าเราอะภาคคอม</li>
+              <li>เว็บก็มี logoก็มี themeก็มี สปอนเซอร์ก็มี สตาฟก็มี ของกินยังมี แต่ขาดอย่างเดียวคือยังไม่มีเธอ...หมายถึงยังไม่มีน้องๆ อย่าลืมมาแล้วชวนเพื่อนมาด้วยนะจ๊ะ</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
