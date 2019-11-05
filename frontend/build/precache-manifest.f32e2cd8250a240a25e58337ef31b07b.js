d0ff680f6e7bcd9a5c199ba07663dac6942b8e0e
phootip <phootip.t@gmail.com>
2019-11-05 17:58:30 +0700

    empty commit

---
+++ frontend/build/precache-manifest.f32e2cd8250a240a25e58337ef31b07b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f006a680244b58b38944",
+    "url": "/connek9-website/static/css/main.18c63756.chunk.css"
+  },
+  {
+    "revision": "f006a680244b58b38944",
+    "url": "/connek9-website/static/js/main.f006a680.chunk.js"
+  },
+  {
+    "revision": "226bec48516f283d6803",
+    "url": "/connek9-website/static/js/runtime~main.226bec48.js"
+  },
+  {
+    "revision": "05be0195ad773f4b1df0",
+    "url": "/connek9-website/static/js/2.05be0195.chunk.js"
+  },
+  {
+    "revision": "c8f672822e8b345a29281d4f0b1801bf",
+    "url": "/connek9-website/static/media/logo-text-black.c8f67282.png"
+  },
+  {
+    "revision": "f9b38d7c0358ecb02c240e6430f19b8b",
+    "url": "/connek9-website/static/media/yannix.f9b38d7c.png"
+  },
+  {
+    "revision": "d1c9e7a9b640d721752601c5340c89ba",
+    "url": "/connek9-website/static/media/sunday.d1c9e7a9.png"
+  },
+  {
+    "revision": "7f580322e34cd72cd9445067e0753e9c",
+    "url": "/connek9-website/static/media/logo-without-text.7f580322.png"
+  },
+  {
+    "revision": "22b6c705f1afac04819a9c57aa513304",
+    "url": "/connek9-website/static/media/boat.22b6c705.svg"
+  },
+  {
+    "revision": "f8cca210909519129c457582afa7bc7e",
+    "url": "/connek9-website/index.html"
+  }
+];
